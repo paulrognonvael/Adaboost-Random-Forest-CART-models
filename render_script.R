d0046e9805abe9ade6a2438ea653b6dfc7dae3bf
Paul Rognon <paul.rognon@gmail.com>
2020-05-05 20:47:42 +0200

    Global update of project

--- render_script.R
+++ render_script.R
@@ -1,4 +1,11 @@
-
-path="~/Documents/MESIO/Statistical Learning/5_Modelos_basados_en_arboles/Classification-tree-model"
-rmarkdown::render("Task_1_v2.Rmd",output_file = paste0(path,"/Task1_Group3_pdf.pdf"))
-rmarkdown::render("Task_1_v2.Rmd",output_file = paste0(path,"/Task1_Group3_html.html"))
+  
+  path="~/Documents/MESIO/Statistical Learning/5_Modelos_basados_en_arboles/Classification-tree-model"
+  rmarkdown::render(paste0(path,"/Task_1_v2.Rmd"), output_format =  c("pdf_document"
+                                                        #,"html_document"
+                                                        ),
+                     output_file = paste0(path,"/Task1_Group3.pdf"))
+  rmarkdown::render(paste0(path,"/Task_1_v2.Rmd"), output_format =  c("html_document"
+                                                        #,"pdf_document"
+                                                        ),
+                    output_file = paste0(path,"/Task1_Group3.html"))
+    
